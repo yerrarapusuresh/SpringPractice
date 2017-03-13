43c54740b84178a4bba55b1437e5b45c9ed806dc
Suresh Yerrarapu <yerrarapusuresh@gmail.com>
2017-03-13 10:08:40 +0530

    Application context with preset values

--- src/main/java/com/spring/suresh/SpringDemo/Traingle.java
+++ src/main/java/com/spring/suresh/SpringDemo/Traingle.java
@@ -2,8 +2,16 @@ package com.spring.suresh.SpringDemo;
 
 public class Traingle {
 	
+	private String type;
+	
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
 	public void draw() {
-		System.out.println("draw traingle");
+		System.out.println("draw traingle "+type);
 	}
 
 }
