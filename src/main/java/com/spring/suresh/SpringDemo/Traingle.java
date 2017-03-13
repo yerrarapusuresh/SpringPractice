22ab78cb84933a90d7f15b1cbfbd69a767637648
Suresh Yerrarapu <yerrarapusuresh@gmail.com>
2017-03-13 10:35:52 +0530

    Constructor injection with type

--- src/main/java/com/spring/suresh/SpringDemo/Traingle.java
+++ src/main/java/com/spring/suresh/SpringDemo/Traingle.java
@@ -5,11 +5,17 @@ public class Traingle {
 	private String type;
 	private int height;
 	
+	public Traingle(String type) {
+		this.type = type;
+	}
+	public Traingle(int height) {
+		this.height = height;
+	}
 	public Traingle(String type, int height) {
 		this.type = type;
 		this.height = height;
 	}
-	
+
 	public String getType() {
 		return type;
 	}
